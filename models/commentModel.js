4231094bd8ff4706a72166a970b04bea47095d72
Ralph Molu <rmolu11@gmail.com>
2024-05-11 16:18:19 -0400

    fixes freeze table names in Post model, completed comment model

--- models/commentModel.js
+++ models/commentModel.js
@@ -7,10 +7,42 @@ class Comment extends Model {}
 // columns in the comment table
 Comment.init(
     {
-        id: {},
-        content: {},
-        userID: {},
-        postId: {}
+        id: {
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            primaryKey: true, 
+            autoIncrement: true
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [2], // avoid empty content
+            }
+        },
+        userID: {
+            type: Datatypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        postId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        }
+    },
 
+    {
+        sequelize,
+        timestamps: true,
+        underscored: true,
+        freezeTableName: true,
+        modelName: 'comment'
     }
-)
+);
+
+module.exports = Comment;
--- models/postModel.js
+++ models/postModel.js
@@ -32,6 +32,7 @@ Post.init(
     {
         sequelize,
         timestamps: true,
+        freezeTableName: true,
         underscored: true,
         modelName: 'post'
     }
